046725cc98331550ac4bc3d847bc5ed50b8dfcb4
shhhQuiettt <krzysztof.skro@gmail.com>
2022-07-13 13:30:29 +0200

    Settings and urls configure to serve react

--- spotifyRooms/settings.py
+++ spotifyRooms/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "spotifyRooms.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "frontend", "build")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend", "build", "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
